b3b01a27a872dcfe0303696ed59b125b2b7e3774
Santiago Gonzalez Gartner <santiagogg@gmail.com>
2016-05-02 16:15:11 -0300

    create index response

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
@@ -15,7 +16,7 @@ class ContactController extends Controller
      */
     public function index()
     {
-        //
+        return \App\Contact::all();
     }
 
 
