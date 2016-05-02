0da4e8451b4c9dc6c576c898f66b9a1be54b0849
Santiago Gonzalez Gartner <santiagogg@gmail.com>
2016-05-02 15:44:44 -0300

    add contact model factory using faker

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -19,3 +19,11 @@ $factory->define(App\User::class, function (Faker\Generator $faker) {
         'remember_token' => str_random(10),
     ];
 });
+
+$factory->define(App\Contact::class, function (Faker\Generator $faker) {
+    return [
+        'name' => $faker->name,
+        'email' => $faker->safeEmail,
+        'phone' => $faker->phoneNumber
+    ];
+});
